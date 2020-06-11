0bb18e4ec7f8c37558d625f340113bdbd5399d09
Yukuro <tupledev9@gmail.com>
2020-06-11 21:27:51 +0900

    [Improve] logging level

--- proxy.py
+++ proxy.py
@@ -1,30 +1,22 @@
 import socket
 import sys
-import traceback
 import threading
 import queue
 import logging
 from datetime import datetime, timedelta
 
-max_conn = 5
-buffer_size = 2048
-q = queue.Queue()
-
-# debug
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-
 def start(listening_port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', listening_port))
-        s.listen(max_conn)
+        logging.info('Inisializing Sockets ... Done')
 
-        print("[*] Inisializing Sockets ... Done")
-        print("[*] Sockets Binded Successfully ...")
-        print("[*] Server Started Successfully [ {} ]\n".format(listening_port))
+        s.bind(('', listening_port))
+        logging.info('Sockets Binded Successfully ...')
+
+        s.listen(max_conn)
+        logging.info('Server Started Successfully [ {} ]\n'.format(listening_port))
     except Exception as e:
-        print("[*] Unable to Initialize Socket")
-        print(traceback.format_exc())
+        logging.exception('Unable to Initialize Socket...')
         sys.exit(2)
 
     while 1:
@@ -33,14 +25,14 @@ def start(listening_port):
             data = conn.recv(buffer_size).decode('utf-8')
             conn_time_now = datetime.now()
 
-            # print("RECV: {}".format(data))
+            logging.debug('RECV data: {}'.format(data))
+
             if len(data) > 0 :
                 t = threading.Thread(target=conn_string, args=(conn,data,addr,conn_time_now))
                 t.start()
         except KeyboardInterrupt:
+            logging.info('\nProxy Server Shutting Down ...')
             s.close()
-            print("\n[*] Proxy Server Shutting Down ...")
-            print("[*] Have A Nice Day ...")
             sys.exit(1)
     s.close()
 
@@ -50,18 +42,18 @@ def conn_string(conn, data, addr, conn_time_now):
 
         url = first_line.split(' ')[1]
 
-        http_pos = url.find("://")
+        http_pos = url.find('://')
         if(http_pos==-1):
             temp = url
         else:
             temp = url[(http_pos+3):]
         
-        port_pos = temp.find(":")
+        port_pos = temp.find(':')
 
-        webserver_pos = temp.find("/")
+        webserver_pos = temp.find('/')
         if(webserver_pos == -1):
             webserver_pos = len(temp)
-        webserver = ""
+        webserver = ''
         port = -1
         if(port_pos == -1 or webserver_pos < port_pos):
             port = 80
@@ -70,14 +62,11 @@ def conn_string(conn, data, addr, conn_time_now):
             port = int((temp[(port_pos+1):])[:webserver_pos - port_pos - 1])
             webserver = temp[:port_pos]
 
-        print("Entry conn_string peer {} sock {}".format(conn.getpeername(), conn.getsockname))
+        logging.debug('Entry conn_string | PEER:{} / SOCK:{}'.format(conn.getpeername, conn.getsockname))
 
         proxy_server(webserver, port, conn, data, addr, conn_time_now)
-    except Exception as e:
-        print("Exception occured...")
-        print(traceback.format_exc())
-
-
+    except Exception:
+        logging.exception('Exception occurred...')
 
 def proxy_server(webserver, port, conn, data, addr, conn_time_now):
     try:
@@ -85,33 +74,32 @@ def proxy_server(webserver, port, conn, data, addr, conn_time_now):
         s.connect((webserver, port))
 
         s.send(data.encode('utf-8'))
-        print("DATA:{}\nSent.".format(data.encode('utf-8')))
+        logging.debug('DATA:{}\nSent -> {}:{}.'.format(data.encode('utf-8'), webserver, port))
 
         while 1:
             reply = s.recv(buffer_size)
-            print("RECV:{}".format((reply)))
+            logging.debug('RCV reply:{}'.format(reply))
             if(len(reply) > 0): # can through only HTTP
                 conn.send(reply)
 
-
-                print("[*] Conn_time_now: {} / from IP: {}".format(conn_time_now, s.getsockname()))
+                logging.debug('Conn_time_now: {} / from IP: {}'.format(conn_time_now, s.getsockname()))
 
                 dar = float(len(reply))
                 dar = float(dar / 1024)
-                dar = "%.3s" % (str(dar))
-                dar = "%s KB" % (dar)
+                dar = '%.3s' % (str(dar))
+                dar = '%s KB' % (dar)
                 'Print A Custom Message For Request Complete'
-                print("[*] Request Done: %s => %s <=" % (str(addr[0]), str(dar)))
+                logging.info('Request Done: %s => %s <=' % (str(addr[0]), str(dar)))
 
-                # calculate trust score
-                global pt
+                global pt # one last Request
                 if(conn_time_now != pt):
                     send_queue((conn_time_now, s.getsockname()[0], data))
 
+                # calculate trust score
                 trust_data_now = calc_trust()
 
                 if trust_data_now:
-                    logging.info("Trust Score: {} / IP: {} ({})".format(trust_data_now[0], trust_data_now[1], datetime.now()))
+                    logging.info('[*] Trust Score: {} from {}'.format(trust_data_now[0], trust_data_now[1]))
 
                 pt = conn_time_now
             else:
@@ -120,8 +108,7 @@ def proxy_server(webserver, port, conn, data, addr, conn_time_now):
         s.close()
         conn.close()
     except socket.error as msg:
-        print("Socket Error Occurred")
-        print(traceback.format_exc())
+        logging.info('Socket Error occurred...')
         s.close()
         conn.close()
         sys.exit(1)
@@ -137,7 +124,7 @@ def calc_trust():
     if q_len < 2:
         return None
 
-    logging.info('ready to calculate...')
+    logging.debug('ready to calculate...')
 
     # TODO: more smart method
     f_access_time, f_from_ip, _ = q.get()
@@ -164,16 +151,23 @@ def _evaluate(first_time, second_time):
 
 def main():
     try:
-        listening_port = int(input("[*] Enter Listening Port Number: "))
+        listening_port = int(input('[-] Enter Listening Port Number: '))
     except KeyboardInterrupt:
-        print("\n[*] User Requested An Interrupt")
-        print("[*] Application Exiting...")
+        logging.info('User Requested An Interrupt')
+        logging.info('Application Exiting...')
         sys.exit()
 
     start(listening_port)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # global variables
+    # TODO remove
     trust_score = 0
     pt = datetime.now()
+    max_conn = 5
+    buffer_size = 2048
+    q = queue.Queue()
+
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
     main()
